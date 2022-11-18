81c31449c49b50bba5187994418e826c11ca5959
Sherry <hhlsdkm@gmail.com>
2022-11-18 18:41:57 +0900

    infinite scroll practice

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <div className="App">
         <h1>Infinite SWAPI</h1>
-        <InfinitePeople />
-        {/* <InfiniteSpecies /> */}
+        {/*<InfinitePeople />*/}
+         <InfiniteSpecies />
         <ReactQueryDevtools />
       </div>
     </QueryClientProvider>
--- src/species/InfiniteSpecies.jsx
+++ src/species/InfiniteSpecies.jsx
@@ -1,4 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
+import {useInfiniteQuery} from "react-query";
+
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -7,7 +9,35 @@ const fetchUrl = async (url) => {
   return response.json();
 };
 
-export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+export const InfiniteSpecies = () => {
+  const {data, fetchNextPage, hasNextPage, isLoading, isFetching, isError, error} = useInfiniteQuery(
+    "sw-species",
+    ({pageParam = initialUrl}) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined
+    }
+  );
+
+  if (isLoading) return <div className={"loading"}>Loading</div>
+  if (isError) return <div>{error.toString()}</div>
+
+  return (
+    <>
+      {isFetching && <div className={"loading"}>Loading</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.average_lifespan}
+              />)
+          })
+        })
+        }
+      </InfiniteScroll>
+    </>
+  );
 }
